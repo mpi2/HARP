0cfad1ba9765a0ab2373c3206d2c479888aca4e0
Thomas Lawson <t.lawson@har.mrc.ac.uk>
2014-03-06 16:14:18 +0000

    RunProcessing.py edited so that scaling factors are correct, siah_scale.txt edited to print finished at the end of script

--- RunProcessing.py
+++ RunProcessing.py
@@ -34,24 +34,47 @@ class RunProcessing:
         if not os.path.exists(cropped_path):
             os.makedirs(cropped_path)
 
-        session.write("Autocrop started\n");
+        session.write("Autocrop started\n")
         subprocess.call(["python", dir+"/autocrop.py","-i",param.input_folder,"-o",
                          cropped_path, "-t", "tif"])
-        session.write("Autocrop finished\n");
+        session.write("Autocrop finished\n")
 
         if param.SF2 == "yes" :
-            session.write("Scaling SF2 started\n");
-            pro = subprocess.Popen(["imagej", "-b", dir+"/siah_scale.txt", param.imageJ+":2"])
+            session.write("Scaling SF2 started\n")
+            proSF2 = subprocess.Popen(["imagej", "-b", dir+"/siah_scale.txt", param.imageJ+":0.5"],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+            print "SF2 standard out and error\n"
+            outputSF2 = proSF2.stdout.read()
+            outerrSF2 = proSF2.stderr.read()
+            print outputSF2
+            print outerrSF2
+            session.write("SF2 standard out and error\n")
+            session.write(outputSF2);
+            session.write(outerrSF2);
 
+        # NEED TO PUT INTO FUNCTION
         if param.SF3 == "yes" :
-            session.write("Scaling SF3 started\n");
-            subprocess.Popen(["imagej", "-b", dir+"/siah_scale.txt", param.imageJ+":3"])
+            session.write("Scaling SF3 started\n")
+            proSF3 = subprocess.Popen(["imagej", "-b", dir+"/siah_scale.txt", param.imageJ+":0.3333"],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+            print "SF3 standard out and error\n"
+            outputSF3 = proSF3.stdout.read()
+            outerrSF3 = proSF3.stderr.read()
+            print outputSF3
+            print outerrSF3
+            session.write("SF3 standard out and error\n")
+            session.write(outputSF2);
+            session.write(outerrSF2);
 
         if param.SF4 == "yes" :
             session.write("Scaling SF4 started\n");
-            subprocess.Popen(["imagej", "-b", dir+"/siah_scale.txt", param.imageJ+":4"])
-
-
+            proSF4 = subprocess.Popen(["imagej", "-b", dir+"/siah_scale.txt", param.imageJ+":0.25"],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+            print "SF2 standard out and error\n"
+            outputSF4 = proSF4.stdout.read()
+            outerrSF4 = proSF4.stderr.read()
+            print outputSF4
+            print outerrSF4
+            session.write("#####SF4 standard out and error\n#####")
+            session.write(outputSF4);
+            session.write(outerrSF4);
 
 
 def main():
