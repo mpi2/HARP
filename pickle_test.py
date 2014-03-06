e73c1ec7c6e6abe4d8018843dfa99f3b93110bf9
Thomas Lawson <t.lawson@har.mrc.ac.uk>
2014-03-06 12:09:48 +0000

    Revert "Removed hardcoding"
    This reverts commit 4a337f80138246e8d6c4e7c7c251cb5905a68144.

--- pickle_test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import pickle
-# Have to load the ConfigClass see http://stefaanlippens.net/pickleproblem for reasons
-from Main import ConfigClass
-
-filehandler = open('/home/tom/Desktop/processed recons/20140408_RCAS_17_18.4e_wt_rc/configObject.txt', 'r')
-check = pickle.load(filehandler)
-
-print check.input_folder, check.recon_folder_size
