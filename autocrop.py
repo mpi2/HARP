68abf63f7303802b1fd8a7ed5a6e30dbd6d6c3b0
n.horner <n.horner@har.mrc.ac.uk>
2014-03-18 09:30:40 +0000

    Fixed big in autocrop

--- autocrop.py
+++ autocrop.py
@@ -9,11 +9,12 @@ import argparse
 import os
 import fnmatch
 import numpy as np
-from multiprocessing import Pool
+from multiprocessing import Pool, cpu_count
 import sys
 import time
 import math
 from collections import Counter
+from matplotlib import pyplot as plt
 
 
 imdims = None
@@ -33,7 +34,10 @@ class Processor:
         Reads image into array. Flips it around each time
         with column/row of interest as the top top row
         '''
-        im = Image.open(filename)
+        try:
+            im = Image.open(filename)
+        except OSError as e:
+            print "Processor can't open file. {0}".format(e)
         matrix = np.array(im)
         crops = {}
         crops["x"] = np.std(matrix, axis=0)
@@ -43,16 +47,25 @@ class Processor:
 
 
 class Cropper:
+    '''
+    Runs as a seperate process and performs the crop on a slice and saves it
+    '''
     def __init__(self, crop_box, out_dir):
+        '''
+        @param cropbox: tuple (x,y,width,height)
+        @param out_dir: str(where to save the cropped imgs)
+        '''
         self.crop_box = crop_box
         self.out_dir = out_dir
 
     def __call__(self, img):
         im = Image.open(img)
-        im = im.crop((self.crop_box[0], self.crop_box[1], self.crop_box[2], self.crop_box[3] ))
+
+        imcrop = im.crop((self.crop_box[0], self.crop_box[1], self.crop_box[2], self.crop_box[3] ))
+
         filename = os.path.basename(img)
         crop_out = os.path.join(self.out_dir,filename)
-        im.save(crop_out)
+        imcrop.save(crop_out)
 
 
 def do_the_crop(images, crop_vals, out_dir,  padding=0):
@@ -60,33 +73,24 @@ def do_the_crop(images, crop_vals, out_dir,  padding=0):
     @param: images list of image files
     @param: crop_vals tuple of the crop box (x, y, w, h)
     @param: out_dir str dir to output cropped images
-
     '''
-    print("doing the crop")
     global imdims
-    print imdims
 
+    #Get the distances of the box sides from the sides of the image
     lcrop = crop_vals[0] - padding
     if lcrop < 0: lcrop = 0
-
     tcrop = crop_vals[1] - padding
     if tcrop < 0: tcrop = 0
-
-    #Get distance from left side
-    #rcrop = imdims[0] - crop_vals[2] + padding
     rcrop = crop_vals[2] + padding
     if rcrop > imdims[0] -1: rcrop = imdims[0] -1
-
-    #get distance from top
-    #bcrop = imdims[1] - crop_vals[3] + padding
     bcrop = crop_vals[3] + padding
     if bcrop > imdims[1] -1: bcrop = imdims[1] -1
 
-    print("cropping with the following box: left:{0}, top:{1}, right:{2}, bottom{3}".format(
+    print("cropping with the following box x1, y1, x2, y2 ({0},{1},{2},{3});".format(
         lcrop, tcrop, rcrop, bcrop))
-    print("ImageJ friendly box: makeRectangle({0},{1},{2},{3})".format(
-        lcrop, tcrop, rcrop -lcrop, bcrop - tcrop))
-
+    print("ImageJ: makeRectangle({0},{1},{2},{3});".format(
+        lcrop, tcrop, rcrop - lcrop, bcrop - tcrop))
+    print lcrop, tcrop, rcrop, bcrop
     cropper = Cropper((lcrop, tcrop, rcrop, bcrop), out_dir)
     pool = Pool()
     pool.map(cropper, images)
@@ -94,14 +98,14 @@ def do_the_crop(images, crop_vals, out_dir,  padding=0):
 
 
 def get_cropping_box(slices, side, threshold, rev = False):
-
-    #Vals = array of arrays with each sub-array containing the
-    # the metric calculated in Process for each row/column
-    #vals = [x[side] for x in slices]
+    '''
+    '''
     vals = [lowvals(x[side]) for x in slices]
 
     #filterout low values
     means = map(np.std, zip(*vals))
+    plt.plot(means)
+    plt.show()
 
     if rev == True:
         return next((i for i, v in enumerate(reversed(means)) if v > threshold ), -1)
@@ -123,8 +127,11 @@ def round_down(array, divisor):
         yield n - (n%divisor)
 
 
-def lowvals(array):
-    low_values_indices = array < 10  # Where values are low
+def lowvals(array, value=20):
+    '''
+    Values lower than value, set to zero
+    '''
+    low_values_indices = array < value
     array[low_values_indices] = 0
     return array
 
@@ -132,8 +139,8 @@ def lowvals(array):
 
 
 def run(args):
-    #Get the file list exclude ones we dont want
-    #Note fnmatch is case insensitive by default so with fnd BMP and bmp
+    '''
+    '''
     files = []
 
 
@@ -149,43 +156,56 @@ def run(args):
     img = Image.open(files[0])
     global imdims
     imdims = img.size
-    #map(open_files, files)
-    #sys.exit()
 
     if args.def_crop:
-        do_the_crop(files, args.def_crop, args.out_dir)
-        sys.exit()
-    else: #Do the autocrop
-        print("No region specified. Trying autocrop")
+        do_the_crop(files, concertXYWH_ToCoords(args.def_crop), args.out_dir)
+        #sys.exit()
+    else:
+        print("Doing autocrop")
         threshold = 0.01
         proc = Processor(threshold)
-        pool = Pool(8)
-        #Just use a subset of files to determine crop
+        pool_num = 0
+        if args.num_proc:
+            pool_num = args.num_proc
+        else:
+            pool_num = cpu_count() -1
+        pool = Pool(pool_num)
+
+        #Take a subset of files and farm off to processes to determine crop vals
         sparse_files = files [0::10]
         slices = pool.map(proc, sparse_files)
-        #end = time.time()
-        #curr_time = end - start
-        #print("Files read and means calculated. Time to here: {0}".format(curr_time) )
-        #print slices
-        #sys.exit()
 
         #Distances of cropping boxes from their respective sides
         ldist = get_cropping_box(slices, "x", threshold)
         tdist = get_cropping_box(slices, "y", threshold)
         rdist = get_cropping_box(slices, "x", threshold, True)
         bdist = get_cropping_box(slices, "y", threshold, True)
-        cropBox = convertDistFromEdgesToXYWH((ldist, tdist, rdist, bdist))
+        cropBox = convertDistFromEdgesToCoords((ldist, tdist, rdist, bdist))
 
         padding = int(np.mean(imdims)*0.01)
         do_the_crop(files, cropBox, args.out_dir, padding)
-        sys.exit()
+        sys.exit(0)
 
 
-def convertDistFromEdgesToXYWH(distances):
+def convertDistFromEdgesToCoords(distances):
+    '''
+    Convert distances from sides(which comes from auto detection) sides into x,y,w,h
+    PIL uses actual coords not with height
+    '''
     global imdims
-    width = imdims[0] - distances[2]
-    height = imdims[1] - distances[3]
-    return((distances[0], distances[1], width, height))
+    x1 = imdims[0] - distances[2]
+    x2 = imdims[1] - distances[3]
+    return((distances[0], distances[1], x1, x2))
+
+
+def concertXYWH_ToCoords(xywh):
+    '''
+    The input dimensions from the GUI needs converting for PIL
+    '''
+    global imdims
+    x1 = xywh[0] + xywh[2]
+    x2 = xywh[1] + xywh[3]
+    return((xywh[0], xywh[1], x1, x2))
 
 
 def main():
@@ -199,8 +219,8 @@ def main():
     parser.add_argument('-i', dest='in_dir', help='dir with bmps to crop', required=True)
     parser.add_argument('-o', dest='out_dir', help='destination for cropped images', required=True)
     parser.add_argument('-t', dest='file_type', help='tif or bmp', default="bmp")
-    parser.add_argument('-d', nargs=4, type=int, dest='def_crop', help='set defined boundaries for crop')
-    parser.add_argument('-p', dest='p', help='set defined boundaries for crop')
+    parser.add_argument('-d', nargs=4, type=int, dest='def_crop', help='set defined boundaries for crop x,y,w,h')
+    parser.add_argument('-p', dest="num_proc", help='number of processors to use')
     args = parser.parse_args()
     run(args)
     #sys.exit()
