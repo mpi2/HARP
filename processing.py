4c7e0d24190e98e0740e3c807815418fefd5a2f8
Thomas Lawson <tomnl@tom>
2014-06-02 12:59:56 +0100

    no crop update

--- processing.py
+++ processing.py
@@ -80,7 +80,7 @@ class ProcessingThread(QtCore.QThread):
             self.emit( QtCore.SIGNAL('update(QString)'), "No Crop carried out" )
             #print "No crop carried out"
             self.session_log.write("No crop carried out\n")
-            autocrop_update_slot("sucess")
+            self.autocrop_update_slot("success")
             return
 
         # Make new crop directory
