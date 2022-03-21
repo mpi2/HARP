cc772467be699c8b7779fbc3d439d5dc30390d85
Henrik Westerberg <h.westerberg@har.mrc.ac.uk>
2022-03-21 13:50:27 +0000

    updating version

--- harp/version.py
+++ harp/version.py
@@ -1,2 +1,3 @@
-__version__ = '2.4.0.4' # Fixed header issue
+__version__ = '2.4.0.5' # Windows install release
+# __version__ = '2.4.0.4' # Fixed header issue
 # __version__ = '2.4.0.2' # Ported to Python3 $ Qt5
