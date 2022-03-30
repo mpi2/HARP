5a299b16cebdb8fc1d2bf6f40200926ef76ef993
Henrik Westerberg <h.westerberg@har.mrc.ac.uk>
2022-03-30 22:51:51 +0100

    Fixed bz2 header issue

--- harp/version.py
+++ harp/version.py
@@ -1,3 +1,4 @@
-__version__ = '2.4.0.5' # Windows install release
+__version__ = '2.4.1' # finally fixed header issue
+# __version__ = '2.4.0.5' # Windows install release
 # __version__ = '2.4.0.4' # Fixed header issue
 # __version__ = '2.4.0.2' # Ported to Python3 $ Qt5
